75c31780e8ef0f9c8287d4cf3c4ef01f8d6d21c2
Egor GoshaTilted <gosha228007@gmail.com>
2022-12-06 21:32:01 +0200

    8 commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import { useTelegram } from './hooks/useTelegram';
 
 const tg = window.Telegram.WebApp;
 
 function App() {
+  const {onToggleButton,tg} = useTelegram()
+    
   useEffect(()=>{
     tg.ready();
   },[])
-
-
+  
   return (
     <div className="App">
-      work
-      <Header></Header>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
