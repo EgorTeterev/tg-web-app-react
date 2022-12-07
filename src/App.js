086618c45caafd25a35b2880b4023e0a1a70ed79
Egor GoshaTilted <egorteterevfs@gmail.com>
2022-12-07 17:07:22 +0200

    x

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import { useEffect } from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import { useTelegram } from './hooks/useTelegram';
-import { Route,Routes} from 'react-router-dom'
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
 
-function App() {
-  const {onToggleButton,tg} = useTelegram()
-    
-  useEffect(()=>{
-    tg.ready();
-  },[])
-
-  return (
-    <div className="App">
-      <Header/>
-      <Routes>
-        <Route index element={<ProductList/>}/>
-        <Route path={'form'} element={<Form/>}/>
-      </Routes>
-    </div>
-  );
-}
-
-export default App;
+  <React.StrictMode>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+  </React.StrictMode>
+);
